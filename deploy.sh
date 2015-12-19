d051396cf56da357975b00155c3b40a99b661577
Sumanth <sumansn@thoughtworks.com>
2015-12-18 18:43:15 -0600

    Added a deploy script

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo Pushing code to heroku
+git push heroku master
+
+echo Stopping dynos
+heroku ps:scale web=0
+heroku ps:scale upgrade=0
+
+echo Upgrading database
+heroku ps:scale upgrade=1
+
+echo Starting dynos
+heroku ps:scale web=1
+
+echo Done
+
