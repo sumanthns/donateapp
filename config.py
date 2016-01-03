4dd06b87c5c9269d32085246d717c908a8681602
Sumanth <sumansn@thoughtworks.com>
2016-01-03 01:28:59 -0600

    Making email work

--- config.py
+++ config.py
@@ -11,15 +11,16 @@ SECRET_KEY = str(uuid.uuid4())
 HOSTNAME = os.environ.get("HOSTNAME")
 
 # email server
-MAIL_SERVER = 'smtp.gmail.com'
+MAIL_SERVER = 'smtp.googlemail.com'
 MAIL_PORT = 465
 MAIL_USE_TLS = False
 MAIL_USE_SSL = True
 MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
 MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+MAIL_DEFAULT_SENDER = os.environ.get('MAIL_ADMIN', "from@example.com")
 
 # administrator list
-ADMINS = [os.environ.get('MAIL_ADMIN')]
+ADMINS = [os.environ.get('MAIL_ADMIN', "from@example.com")]
 
 #payment gateway info
 PAYMENT_GW_API_KEY = os.environ.get("PAYMENT_GW_API_KEY")
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export DATABASE_URL="mysql://root@localhost/donateapp"
-export PAYMENT_GW_API_KEY = 'e73d335de204715e2b05446d0191e3dc'
-export PAYMENT_GW_AUTH_TOKEN = '1ff3c01b1f8862fc7a3bf0594594591b'
-export PAYMENT_GW_SALT = 'de113111b6ff4f1ea43613623a742d66'
+export PAYMENT_GW_API_KEY='e73d335de204715e2b05446d0191e3dc'
+export PAYMENT_GW_AUTH_TOKEN='1ff3c01b1f8862fc7a3bf0594594591b'
+export PAYMENT_GW_SALT='de113111b6ff4f1ea43613623a742d66'
 gunicorn --workers=1 --threads=5 --log-level=debug app:app
